8d2dc790cc96c55dfba3b3f077b4a058d1b90e28
SilverArgente <53583492+SilverArgente@users.noreply.github.com>
2023-05-18 16:57:33 -0400

    Added mode switching funtionality
    Can now switch between stopwatch and timer, timer functionality is unfinished however

--- src/main/java/com/example/studytimer/Timer.java
+++ src/main/java/com/example/studytimer/Timer.java
@@ -26,17 +26,19 @@ import javafx.util.Duration;
 
 public class Timer extends Application {
 
-    // Init variables
-    Stage window;
-
+    // Visual component variables
+    Scene scene;
     Label clock = new Label("00:00:00");
+    Button stopwatchBtn, timerBtn, play, reset;
+    StackPane timerDisplay;
+    HBox topBar, bottomBar;
+    BorderPane layout;
 
+    // Timer functionality variables
     static boolean timerIsRunning;
-
     private static final Duration INTERVAL = Duration.seconds(1);
     long elapsedTime = 0;
     long timeAtPause = 0;
-
     private long startTime;
     private Timeline timeline;
 
@@ -49,53 +51,80 @@ public class Timer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
+        // Adding CSS file doesn't work for some reason, time to hard code it
+        // scene.getStylesheets().add("/src/css/style.css");
         Parent root = FXMLLoader.load(getClass().getResource("study_timer.fxml"));
 
-        // String stylesheet = getClass().getResource("/style.css").toExternalForm();
-        window = primaryStage;
+        // Final display to add to scene
+        layout = new BorderPane();
 
+        // Set stopwatch as default mode
+        initializeStopwatchScene();
+
+        // Switch between stopwatch and timer
+        stopwatchBtn.setOnAction(e -> {
+            initializeStopwatchScene();
+        });
+
+        timerBtn.setOnAction(e -> {
+            initializeTimerScene();
+        });
+
+        // Apply final layout to scene, display scene to end user
+        scene = new Scene(layout, 500, 500);
+        
+        primaryStage.setTitle("Study Timer");
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(300);
+        primaryStage.setMaxHeight(500);
+        primaryStage.setMaxWidth(500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    private void initializeStopwatchScene() {
         // Stopwatch/Timer Buttons layout component
-        HBox topBar = new HBox();
+        topBar = new HBox();
         topBar.setPadding(new Insets(15, 12, 15, 12));
         topBar.setSpacing(30);
 
         // Stopwatch Mode Button
-        Button stopwatchBtn = new Button("Stopwatch");
+        stopwatchBtn = new Button("Stopwatch");
         stopwatchBtn.getStyleClass().add("bg-1");
 
         // Timer Mode Button
-        Button timerBtn = new Button("Timer");
+        timerBtn = new Button("Timer");
         timerBtn.getStyleClass().add("bg-1");
 
         topBar.getChildren().addAll(stopwatchBtn, timerBtn);
         topBar.setAlignment(Pos.TOP_CENTER);
 
         // Timer Display layout component
-        StackPane timerDisplay = new StackPane();
+        timerDisplay = new StackPane();
         clock.setFont(new Font("Arial", 26));
         clock.getStyleClass().add("bg-1");
         timerDisplay.getChildren().addAll(clock);
 
         // Start / Pause / Reset Button layout component
-        HBox bottomBar = new HBox();
+        bottomBar = new HBox();
         bottomBar.setPadding(new Insets(15, 12, 15, 12));
         bottomBar.setSpacing(30);
         bottomBar.setAlignment(Pos.TOP_CENTER);
         bottomBar.setStyle("-fx-background-color: #909090;");
 
-
-
         // Play / Pause button
-        Button play = new Button("Play");
+        play = new Button("Play");
         play.getStyleClass().add("bg-2");
 
         // Reset button
-        Button reset = new Button("Reset");
+        reset = new Button("Reset");
         reset.getStyleClass().add("bg-1");
 
-
-        // Add both buttons to bottom bar layout component
-        bottomBar.getChildren().addAll(play, reset);
+        layout.setTop(topBar);
+        layout.setCenter(timerDisplay);
+        layout.setBottom(bottomBar);
 
         // If Play Button is clicked
         play.setOnAction(e -> {
@@ -120,35 +149,114 @@ public class Timer extends Application {
             clock.setText("00:00:00");
         });
 
+        // Switch between stopwatch and timer
+        stopwatchBtn.setOnAction(e -> {
+            initializeStopwatchScene();
+        });
 
-        // Embed all layout components into one layout to establish scnee
-        BorderPane layout = new BorderPane();
+        timerBtn.setOnAction(e -> {
+            initializeTimerScene();
+        });
+
+        // Add both buttons to bottom bar layout component
+        bottomBar.getChildren().addAll(play, reset);
+
+        // Set theme to all components
+        setStopwatchModeTheme();
+    }
+
+    private void initializeTimerScene() {
+        // Stopwatch/Timer Buttons layout component
+        topBar = new HBox();
+        topBar.setPadding(new Insets(15, 12, 15, 12));
+        topBar.setSpacing(30);
+
+        // Stopwatch Mode Button
+        stopwatchBtn = new Button("Stopwatch");
+        stopwatchBtn.getStyleClass().add("bg-1");
+
+        // Timer Mode Button
+        timerBtn = new Button("Timer");
+        timerBtn.getStyleClass().add("bg-1");
+
+        topBar.getChildren().addAll(stopwatchBtn, timerBtn);
+        topBar.setAlignment(Pos.TOP_CENTER);
+
+        // Timer Display layout component
+        timerDisplay = new StackPane();
+        clock.setFont(new Font("Arial", 26));
+        clock.getStyleClass().add("bg-1");
+        timerDisplay.getChildren().addAll(clock);
+
+        // Start / Pause / Reset Button layout component
+        bottomBar = new HBox();
+        bottomBar.setPadding(new Insets(15, 12, 15, 12));
+        bottomBar.setSpacing(30);
+        bottomBar.setAlignment(Pos.TOP_CENTER);
+        bottomBar.setStyle("-fx-background-color: #909090;");
+
+        // Play / Pause button
+        play = new Button("Play");
+        play.getStyleClass().add("bg-2");
+
+        // Reset button
+        reset = new Button("Reset");
+        reset.getStyleClass().add("bg-1");
+
         layout.setTop(topBar);
         layout.setCenter(timerDisplay);
         layout.setBottom(bottomBar);
 
+        // If Play Button is clicked
+        play.setOnAction(e -> {
+            // Play
+            if (!timerIsRunning) {
+                play.setText("Pause");
+                startTimer();
+            }
+            // Stop
+            else {
+                play.setText("Play");
+                stopTimer();
+            }
+        });
 
-        // scene.getStylesheets().add("style.css")
+        // If reset button is clicked
+        reset.setOnAction(e -> {
+            play.setText("Pla");
+            stopTimer();
+            elapsedTime = 0;
+            timeAtPause = 0;
+            clock.setText("00:00:00");
+        });
 
-        // Adding CSS file doesn't work for some reason, time to hard code it
-        // scene.getStylesheets().add("/src/css/style.css");
+        // Switch between stopwatch and timer
+        stopwatchBtn.setOnAction(e -> {
+            initializeStopwatchScene();
+        });
 
-        // CSS Stylings
-        layout.setStyle("-fx-background-color: #192428");
-        topBar.setStyle("-fx-background-color: #414C50");
-        bottomBar.setStyle("-fx-background-color: #414C50");
+        timerBtn.setOnAction(e -> {
+            initializeTimerScene();
+        });
 
+        // Add both buttons to bottom bar layout component
+        bottomBar.getChildren().addAll(play, reset);
+
+        setTimerModeTheme();
+    }
+
+    public void setStopwatchModeTheme() {
         stopwatchBtn.setStyle(
-        "-fx-background-color: #39ACE7;" +
-        "-fx-border: none;" +
-        "-fx-text-color: white;" +
-        "-fx-padding: 10px 24px;" +
-        "-fx-text-align: center;" +
-        "-fx-text-decoration: none;" +
-        "-fx-display: inline-block;" +
-        "-fx-font-size: 16px;" +
-        "-fx-margin: 4px 2px;" +
-        "-fx-cursor: pointer"
+                "-fx-background-color: #2A7BA3;" +
+                        "-fx-border: none;" +
+                        "-fx-text-color: white;" +
+                        "-fx-padding: 10px 24px;" +
+                        "-fx-text-align: center;" +
+                        "-fx-text-decoration: none;" +
+                        "-fx-display: inline-block;" +
+                        "-fx-font-size: 16px;" +
+                        "-fx-margin: 4px 2px;" +
+                        "-fx-cursor: pointer"
         );
 
         timerBtn.setStyle(
@@ -190,18 +298,76 @@ public class Timer extends Application {
                         "-fx-cursor: pointer"
         );
 
+        layout.setStyle("-fx-background-color: #192428");
+        topBar.setStyle("-fx-background-color: #414C50");
+        bottomBar.setStyle("-fx-background-color: #414C50");
+
+        reset.setVisible(true);
+
         clock.setStyle("-fx-text-fill: white");
 
-        Scene scene = new Scene(layout, 500, 500);
+    }
 
-        window.setTitle("Study Timer");
-        window.setMinHeight(300);
-        window.setMinWidth(300);
-        window.setMaxHeight(500);
-        window.setMaxWidth(500);
-        window.setScene(scene);
-        window.show();
+    public void setTimerModeTheme () {
+        stopwatchBtn.setStyle(
+                "-fx-background-color: #39ACE7;" +
+                        "-fx-border: none;" +
+                        "-fx-text-color: white;" +
+                        "-fx-padding: 10px 24px;" +
+                        "-fx-text-align: center;" +
+                        "-fx-text-decoration: none;" +
+                        "-fx-display: inline-block;" +
+                        "-fx-font-size: 16px;" +
+                        "-fx-margin: 4px 2px;" +
+                        "-fx-cursor: pointer"
+        );
 
+        timerBtn.setStyle(
+                "-fx-background-color: #2A7BA3;" +
+                        "-fx-border: none;" +
+                        "-fx-text-color: white;" +
+                        "-fx-padding: 10px 24px;" +
+                        "-fx-text-align: center;" +
+                        "-fx-text-decoration: none;" +
+                        "-fx-display: inline-block;" +
+                        "-fx-font-size: 16px;" +
+                        "-fx-margin: 4px 2px;" +
+                        "-fx-cursor: pointer"
+        );
+
+        play.setStyle(
+                "-fx-background-color: #39ACE7;" +
+                        "-fx-border: none;" +
+                        "-fx-text-color: white;" +
+                        "-fx-padding: 10px 24px;" +
+                        "-fx-text-align: center;" +
+                        "-fx-text-decoration: none;" +
+                        "-fx-display: inline-block;" +
+                        "-fx-font-size: 16px;" +
+                        "-fx-margin: 4px 2px;" +
+                        "-fx-cursor: pointer"
+        );
+
+        reset.setStyle(
+                "-fx-background-color: #39ACE7;" +
+                        "-fx-border: none;" +
+                        "-fx-text-color: white;" +
+                        "-fx-padding: 10px 24px;" +
+                        "-fx-text-align: center;" +
+                        "-fx-text-decoration: none;" +
+                        "-fx-display: inline-block;" +
+                        "-fx-font-size: 16px;" +
+                        "-fx-margin: 4px 2px;" +
+                        "-fx-cursor: pointer"
+        );
+
+        layout.setStyle("-fx-background-color: #192428");
+        topBar.setStyle("-fx-background-color: #414C50");
+        bottomBar.setStyle("-fx-background-color: #414C50");
+
+        reset.setVisible(false);
+
+        clock.setStyle("-fx-text-fill: white");
     }
 
     // Plays timer
